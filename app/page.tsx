1380b9339085981bf6c19584ec43a41d5c392356
Jett Gentes <146028226+arctyx-0@users.noreply.github.com>
2026-03-19 11:15:51 -0700

    Add type assertion for teams array in setTeams

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Bracket() {
 
     if (nextWinner) {
       setTeams(prev => {
-        const next = [...((prev as any)[nextWinner[0]] as [any, any])];
+        const next = [...((prev as any)[nextWinner[0]] as [any, any])] as [any, any];
         next[nextWinner[1]] = winVal;
         return { ...prev, [nextWinner[0]]: next };
       });
@@ -43,7 +43,7 @@ export default function Bracket() {
 
     if (nextLoser) {
       setTeams(prev => {
-        const next = [...((prev as any)[nextLoser[0]] as [any, any])];
+        const next = [...((prev as any)[nextLoser[0]] as [any, any])] as [any, any];
         next[nextLoser[1]] = loseVal;
         return { ...prev, [nextLoser[0]]: next };
       });
